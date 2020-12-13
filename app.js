eaba3220a7696e596cb6c11e57d24984bff54130
Tatiana <Baletrot@gmail.com>
2020-12-13 13:42:01 +0200

    I created first lines of js code

---
+++ app.js
@@ -0,0 +1,18 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+//Event listeners
+todoButton.addEventListener('click', addTodo);
+
+
+
+//Functions
+function addTodo(event) {
+    //prevent form from submitting
+    event.preventDefault();
+
+
+}
